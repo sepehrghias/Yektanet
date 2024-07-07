ac38f82b78415fe03eacebb252ebbe12215adcb1
Sepehr <ghiassepehr@gmail.com>
2024-07-07 01:41:45 -0600

    main change in project

--- advertiser_management/apps.py
+++ advertiser_management/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MangementAdvertiserConfig(AppConfig):
+class AdvertiserManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mangement_advertiser'
+    name = 'advertiser_management'
---
+++ advertiser_management/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def make_ad(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
