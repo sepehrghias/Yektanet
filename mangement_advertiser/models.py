5069aca19791be7d7ae6c296bb4cb45e50b7e63b
Sepehr <ghiassepehr@gmail.com>
2024-07-07 00:54:32 -0600

    make model of this projects

--- mangement_advertiser/models.py
+++ mangement_advertiser/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Advertiser(models.Model):
+    advertiser_name = models.CharField(max_length=200)
+    number_of_advertisements = models.IntegerField(default=0)
+
+class Ad(models.Model):
+    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
+    content = models.TextField()
+    priority = models.IntegerField(default=0)
