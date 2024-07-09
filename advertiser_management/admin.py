4fe985fde1f88df31804c9a6461b83e3652c727f
Sepehr <ghiassepehr@gmail.com>
2024-07-09 11:07:19 +0330

    make admin pannel

--- advertiser_management/admin.py
+++ advertiser_management/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 from advertiser_management.models import Advertiser, Ad, View , Click
 
+class AdAdmin(admin.ModelAdmin):
+    list_filter = ('approve' ,)
+    search_fields = ('title', )
+
+
 # Register your models here.
 admin.site.register(Advertiser)
-admin.site.register(Ad)
+admin.site.register(Ad,AdAdmin)
 admin.site.register(View)
 admin.site.register(Click)
