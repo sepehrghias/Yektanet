04ae21fa5ec1abc99d2b230be8518c9478d83e06
Sepehr <ghiassepehr@gmail.com>
2024-07-10 10:43:49 +0330

    make generic class

--- advertiser_management/urls.py
+++ advertiser_management/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from advertiser_management import views
-from advertiser_management.views import AdvertiserList , CreatingForm
+from advertiser_management.views import AdvertiserList , CreatingForm , AdReportView
 app_name = 'advertiser_management'
 urlpatterns = [
     path("", AdvertiserList.as_view(), name="index"),
     path("create/", CreatingForm.as_view(), name="create_ad"),
     path("<int:ad_id>/", views.click, name="click"),
-    path("report/", views.report,name="report"),
+    path("report/", AdReportView.as_view() ,name="report"),
 ]
--- advertiser_management/views.py
+++ advertiser_management/views.py
@@ -37,50 +37,65 @@ def click(request , ad_id):
     new_click = Click.objects.create(ad=ad, click_date=timezone.now(), ip_address=request.ip)
     return redirect(ad.landing_url)
 
-def report(request):
-    ads = Ad.objects.all()
-    report = []
+class AdReportView(ListView):
+    model = Ad
+    template_name = 'advertiser_management/report.html'
+    context_object_name = 'ads'
 
-    for ad in ads:
-        clicks_per_hour = Click.objects.filter(ad_id=ad.id).annotate(hour=TruncHour('click_date')).values('hour').annotate(clicks_count=Count('ad_id'))
-        views_per_hour = View.objects.filter(ad_id=ad.id).annotate(hour=TruncHour('view_date')).values('hour').annotate(views_count=Count('ad_id'))
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        ads = context['ads']
+        report = []
 
-        total_clicks = Click.objects.filter(ad=ad).count()
-        total_views = View.objects.filter(ad=ad).count()
-        ctr = (total_clicks / total_views) if total_views > 0 else 0
+        for ad in ads:
+            # Get clicks per hour
+            clicks_per_hour = Click.objects.filter(ad=ad).annotate(
+                hour=TruncHour('click_date')
+            ).values('hour').annotate(click_count=Count('id')).order_by('hour')
 
-        avg_time_diff = Click.objects.filter(ad=ad).annotate(
-            time_diff=ExpressionWrapper(F('click_date') - F('ad__views__view_date'),
-                                        output_field=fields.DurationField())).aggregate(Avg('time_diff'))
+            # Get views per hour
+            views_per_hour = View.objects.filter(ad=ad).annotate(
+                hour=TruncHour('view_date')
+            ).values('hour').annotate(view_count=Count('id')).order_by('hour')
 
-        # Create a dictionary for easier lookup
-        clicks_dict = {item['hour']: item['clicks_count'] for item in clicks_per_hour}
-        views_dict = {item['hour']: item['views_count'] for item in views_per_hour}
+            total_clicks = Click.objects.filter(ad=ad).count()
+            total_views = View.objects.filter(ad=ad).count()
+            ctr = (total_clicks / total_views) if total_views > 0 else 0
 
-        # Calculate CTR per hour
-        ctr_per_hour = []
-        all_hours = sorted(set(clicks_dict.keys()).union(set(views_dict.keys())))
-        for hour in all_hours:
-            hour_clicks = clicks_dict.get(hour, 0)
-            hour_views = views_dict.get(hour, 0)
-            hour_ctr = (hour_clicks / hour_views) if hour_views > 0 else 0
-            ctr_per_hour.append({
-                'hour': hour,
-                'clicks': hour_clicks,
-                'views': hour_views,
-                'ctr': hour_ctr,
+            avg_time_diff = Click.objects.filter(ad=ad).annotate(
+                time_diff=ExpressionWrapper(
+                    F('click_date') - F('ad__views__view_date'), output_field=fields.DurationField()
+                )
+            ).aggregate(Avg('time_diff'))
+
+            # Create a dictionary for easier lookup
+            clicks_dict = {item['hour']: item['click_count'] for item in clicks_per_hour}
+            views_dict = {item['hour']: item['view_count'] for item in views_per_hour}
+
+            # Calculate CTR per hour
+            ctr_per_hour = []
+            all_hours = sorted(set(clicks_dict.keys()).union(set(views_dict.keys())))
+            for hour in all_hours:
+                hour_clicks = clicks_dict.get(hour, 0)
+                hour_views = views_dict.get(hour, 0)
+                hour_ctr = (hour_clicks / hour_views) if hour_views > 0 else 0
+                ctr_per_hour.append({
+                    'hour': hour,
+                    'clicks': hour_clicks,
+                    'views': hour_views,
+                    'ctr': hour_ctr,
+                })
+
+            report.append({
+                'ad': ad,
+                'total_clicks': total_clicks,
+                'total_views': total_views,
+                'clicks_per_hour': clicks_per_hour,
+                'views_per_hour': views_per_hour,
+                'ctr': ctr,
+                'avg_time_diff': avg_time_diff['time_diff__avg'],
+                'ctr_per_hour': ctr_per_hour,
             })
 
-        report.append({
-            'ad': ad,
-            'clicks_per_hour': clicks_per_hour,
-            'views_per_hour': views_per_hour,
-            'ctr': ctr,
-            'avg_time_diff': avg_time_diff['time_diff__avg'],
-            'ctr_per_hour': ctr_per_hour,
-        })
-
-    context = {
-        'report': report,
-    }
-    return render(request, 'advertiser_management/report.html', context)
+        context['report'] = report
+        return context
